62684854eef9081920410224d5db3414d37db3ea
d0dam <smallkdb@gmail.com>
2024-11-12 12:01:50 +0900

    chore: initial setting

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript-movie
+
+5주차 비동기 실습 과제
---
+++ src/types/movie.d.ts
@@ -0,0 +1,59 @@
+export interface MovieAPISuccess {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface MovieAPIFailure {
+  status_message: string;
+  status_code: number;
+  success?: boolean;
+}
+
+export type MovieAPIResponse = MovieAPISuccess | MovieAPIFailure;
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieDetail {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: BelongsToCollection;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
